22c9196e4e2996476160430c1edd732d075fcd87
Matias <matiii.rossi2003@gmail.com>
2025-10-20 19:03:23 -0300

    Opcion 1

--- principal.py
+++ principal.py
@@ -1,5 +1,6 @@
 import clase
 
+
 def calcular_comision(envio):
     m = envio.monto
     alg = envio.comision
@@ -32,6 +33,7 @@ def calcular_comision(envio):
             c = 50000
     return c
 
+
 def calcular_impuesto(envio, base):
     alg = envio.impositivo
     imp = 0
@@ -50,6 +52,7 @@ def calcular_impuesto(envio, base):
         imp = 0.03 * base
     return imp
 
+
 def monto_final(envio):
     com = calcular_comision(envio)
     base = envio.monto - com
@@ -58,80 +61,111 @@ def monto_final(envio):
     final_pago = final_origen * envio.tasa
     return com, imp, final_pago
 
+ 
 def menu():
-    print('1) Cargar Envíos')
-    print('2) Mostrar Resultados')
-    print('0) Salir')
-    print()
+    print('1. Cargar envíos')
+    print('2. Mostrar listado')
+    print('3. Buscar')
+    print('4. Mayores')
+    print('0. Salir')
     return int(input('Ingrese opción: '))
 
+
+def add_in_order(v, envio):
+    n = len(v)
+    izq, der = 0, n - 1
+    while izq  <= der:
+        c = (izq + der) // 2
+        if v[c].identificacion_destinatario == envio.identificacion_destinatario:
+            pos = c
+            break
+        elif  v[c].identificacion_destinatario < envio.identificacion_destinatario:
+            der = c - 1
+        else:
+            izq = c + 1
+
+    if izq > der:
+        pos = izq
+    v[pos:pos] = [envio]
+    return v
+
+
+def cargar_envios(v):
+    # procesar archivo .csv...
+    with open('envios.csv', 'r') as f:
+        lineas = f.readlines()  # guardar contenido...
+    v = []  # vaciar arreglo de envios...
+    for linea in lineas:  # recorrer cada envio por separado...
+        linea = linea.rstrip("\n")  # elimina el salto de linea al final de cada envio...
+        datos = linea.split(",")  # separa los datos por las comas (formato .csv)...
+
+        # porcesar datos del envio...
+        cod = datos[0]
+        des = datos[1]
+        nom = datos[2]
+        tas = datos[3]
+        mon = datos[4]
+        com = datos[5]
+        imp = datos[6]
+
+        # generar un objeto/registro envío a partir de los datos obtenidos...
+        envio = clase.Envio(cod, des, nom, tas, mon, com, imp)
+        # agregar en su lugar correspondiente al nuevo objeto...
+        v = add_in_order(v, envio)
+    return v
+
+
+def mostrar(v):
+    # solicitar indice del arreglo cargado...
+    n = int(input("Ingresar índice: "))
+
+    # obtener identificador de pago del envio con el indice "n"...
+    id_pago = v[n].obtener_identificador_pago()
+    print("r1.1:", id_pago)
+    # verificar impar...
+    if n % 2 == 1:
+        n = 3 * n + 1  # nuevo indice...
+    # verificar par...
+    elif n % 2 == 0:
+        n = n // 2  # nuevo indice...
+    # verificar indice fuera de rango...
+    rango = len(v)
+    if n <= rango:
+        id_pago = v[n].obtener_identificador_pago()
+        print("r1.2:", id_pago)
+    # mostrar ultimo elemento (fuera de rango)...
+    else:
+        id_pago = v[-1].obtener_identificador_pago()
+        print("r1.2:", id_pago)
+        # print("r1.2:", v[-1])
+
+
 def principal():
-    envios = []
-    op = -1
+    v = []  # inicializar arreglo...
+    op = -1  # forzar primera vuelta...
     while op != 0:
-        op = menu()
-        if op == 1:
-            with open('envios.csv', 'r') as f:
-                lineas = f.readlines()
-            envios = []
-            c1 = 0
-            c2 = 0
-            for linea in lineas:
-                linea = linea.rstrip("\n")
-                datos = linea.split(",")
-                cod = datos[0]
-                des = datos[1]
-                nom = datos[2]
-                tas = datos[3]
-                mon = datos[4]
-                com = datos[5]
-                imp = datos[6]
-                envio = clase.Envio(cod, des, nom, tas, mon, com, imp)
-                envios.append(envio)
-                c1 += 1
-                if envio.moneda_origen != envio.moneda_pago:
-                    c2 += 1
-            print("r1.1:", c1)
-            print("r1.2:", c2)
+        op = menu()  # menu de control del programa...
+        if op == 1:  # opcion 1 (Cargar Envios)...
+            """  ==> PARTE 1 <==
+            Debe importar los envíos del archivo “envíos.csv” (que es exactamente el mismo que el usado 
+            en el TP3) y agregarlos a un vector, de tal manera que el vector se mantenga siempre 
+            ordenado por identificación del destinatario, de forma descendente. """
+            v = cargar_envios(v)
 
-        elif op == 2:
-            total_comision = 0
-            mayor_descuento = -1
-            envio_mayor = None
+            """  ==> PARTE 2 <==
+            Cuando haya terminado de cargar el arreglo, y antes de salir de la opción 1 del menú, debe 
+            solicitar al usuario que ingrese por teclado un valor i que hace referencia a un índice del 
+            arreglo que se ha cargado. Y con estos elementos, mostrar las siguientes salidas: """
+            mostrar(v)
 
-            for e in envios:
-                com, imp, final = monto_final(e)
-                porc_com = (com / e.monto) * 100
-                total_comision += porc_com
-                descuento = ((com + imp) / e.monto) * 100
-                if descuento > mayor_descuento:
-                    mayor_descuento = descuento
-                    envio_mayor = e
-                    monto_final_mayor = final
+        elif op == 2:  # opcion 2 (Generar Archivo)...
+            pass
+        
+        elif op == 3:  # opcion 3 (Buscar Envio)...
+            pass
 
-            prom_com = total_comision / len(envios)
-
-            print("r2.1:", prom_com)
-            print("r2.2:", envio_mayor.id_pago)
-            print("r2.3:", monto_final_mayor)
-
-            combinaciones = []
-            for e in envios:
-                combo = (e.moneda_origen, e.moneda_pago)
-                if combo not in combinaciones:
-                    combinaciones.append(combo)
-
-            for origen, destino in combinaciones:
-                mayor_final = -1
-                id_envio = ""
-                for e in envios:
-                    if e.moneda_origen == origen and e.moneda_pago == destino:
-                        com, imp, final = monto_final(e)
-                        if final > mayor_final:
-                            mayor_final = final
-                            id_envio = e.id_pago
-
-                print(f"Origen {origen} -> Destino {destino}: {mayor_final}")
+        elif op == 4:  # opcion 4 (Mayores por Combinacion de Moneda)...
+            pass
 
 
 if __name__ == "__main__":
